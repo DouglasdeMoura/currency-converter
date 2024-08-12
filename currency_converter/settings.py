cb7e87456da2b14ee129566cee24a1888adb3f67
Douglas Moura <douglas.ademoura@gmail.com>
2024-08-12 09:56:48 -0300

    chore: add localhost to list of allowed hosts

--- currency_converter/settings.py
+++ currency_converter/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DEBUG"] == "True"
 
-ALLOWED_HOSTS = ["testserver"]
+ALLOWED_HOSTS = ["localhost", "testserver"]
 
 
 # Application definition
