e83145a964ac85b7294fe38a9999c232ecca12ea
Douglas Moura <douglas.ademoura@gmail.com>
2024-08-12 22:51:33 -0300

    feat: separate serializers in its own files

---
+++ currency_converter/serializers/__init__.py
@@ -0,0 +1,3 @@
+from currency_converter.serializers.group import GroupSerializer  # noqa: F401
+from currency_converter.serializers.transaction import TransactionSerializer  # noqa: F401
+from currency_converter.serializers.user import UserSerializer  # noqa: F401
--- currency_converter/serializers/transaction.py
+++ currency_converter/serializers/transaction.py
@@ -1,21 +1,8 @@
 from decimal import Decimal
 
-from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 
 from currency_converter import models
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ["url", "username", "email", "groups"]
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ["url", "name"]
 
 
 class TransactionSerializer(serializers.HyperlinkedModelSerializer):
