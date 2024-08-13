f96e969876875e1313fddcb023734fe3c6f60203
Douglas Moura <douglas.ademoura@gmail.com>
2024-08-13 07:40:43 -0300

    chore: add URL to allowed hosts

--- currency_converter/settings.py
+++ currency_converter/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DEBUG"] == "True"
 
-ALLOWED_HOSTS = ["localhost", "testserver", "0.0.0.0"]
+ALLOWED_HOSTS = ["localhost", "testserver", "0.0.0.0", "jaya-challenge-frosty-violet-7263.fly.dev"]
 
 
 # Application definition
