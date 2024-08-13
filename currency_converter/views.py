519240f51ea3a5b44f7bcd0197bfefa34a58bd25
Douglas Moura <douglas.ademoura@gmail.com>
2024-08-12 22:38:26 -0300

    feat: create ViewSet for transactions

--- currency_converter/views.py
+++ currency_converter/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
 
-from currency_converter.serializers import GroupSerializer, UserSerializer
+from currency_converter.models import Transaction
+from currency_converter.serializers import GroupSerializer, TransactionSerializer, UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -22,3 +23,19 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all().order_by("name")
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows transactions to be viewed or edited.
+    """
+
+    queryset = Transaction.objects.all().order_by("timestamp")
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
